d833a9e62f92fc953358b86bf4f384e738610b47
Ata <106282747+atakanberkyilmaz@users.noreply.github.com>
2023-10-09 18:11:42 +0300

    update

--- src/screens/Home/Questions/AddNewModal.js
+++ src/screens/Home/Questions/AddNewModal.js
@@ -1,18 +1,38 @@
 import { View, Text,StyleSheet } from 'react-native'
 import React from 'react'
+import {Box, Heading, Input, Button } from "native-base"
 
 const AddNewModal = () => {
   return (
-    <View style = {styles.container}>
-      <Text>AddNewModal</Text>
-    </View>
+    <Box backgroundColor="#ddd" flex= {"1"}>
+      <Box p={6} flex= {"1"}>
+      <Heading mb="2">Question</Heading>
+      <Input
+      placeholder = "Enter a new question..."
+      fontSize= {20}
+      borderColor = "#686565"
+      />
+
+      <Heading mt={6} mb={2}>Options</Heading>
+      <Input
+      placeholder = "Enter a new question..."
+      fontSize= {18}
+      borderColor = "#686565"
+      mb={1}
+      />
+      <Input
+      placeholder = "Enter a new question..."
+      fontSize= {18}
+      borderColor = "#686565"
+      />
+      <Box mt={3}>
+        <Button colorScheme={"coolGray"}>New Option</Button>
+      </Box> 
+      </Box>
+      <Box>
+        <Button>Save</Button>
+      </Box>
+    </Box>
   )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 10,
-    }
-})
-
 export default AddNewModal
